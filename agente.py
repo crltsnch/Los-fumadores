7429a013c66b7cb28db8f4286ee6e524fef342ef
crltsnch <crltsnch@myuax.com>
2023-05-16 21:12:15 +0200

    main

--- agente.py
+++ agente.py
@@ -4,4 +4,13 @@ class Agente:
     def dejar_ingredientes(self, ingredientes):
         print('Agente inciiado')
         while True:
+            #Elegir dos ingredientes al azar
+            ing1, ing2 = random.sample(['papel', 'tabaco', 'filtros', 'green', 'mechero'], 2)
+            #Agregar los ingredientes a la lista de ingredientes disponibles
+            ingredientes.extend([ing1, ing2])
+            #Esperar a que un fumador termine
+            tarea_completada = app.tasks['agente.tarea_completada']
+            tarea_completada.apply_async().get()
+            print(f'Fumador ha terminado, nueva ronda...')
+            time.sleep(1)
             
